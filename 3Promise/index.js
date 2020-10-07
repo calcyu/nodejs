5ed4afe610d8b9d2bb7a1d76cf23899e6ae1709d
CalcYu <calcyu@qq.com>
2020-10-07 15:05:42 +0800

    promis

--- 3Promise/index.js
+++ 3Promise/index.js
@@ -4,15 +4,29 @@ const cmd = require('readline')
     output: process.stdout
 })
 
-cmd.question('我手机没电了！可以看下你的手机地图？'
-,answer=>{
-    if(answer=='可以'){
-        cmd.question('我想知道怎么走进你的心里！',(answer)=>{
+// cmd.question('我手机没电了！可以看下你的手机地图？'
+// ,answer=>{
+//     if(answer=='可以'){
+//         cmd.question('我想知道怎么走进你的心里！',(answer)=>{
 
+//         })
+//     }
+// });
+//Promise es6
+function quiz(msg){
+    return new Promise((resolve,reject)=>{
+        cmd.question(msg,function(answer){
+            resolve(answer);
+        })
+    })
+}
+quiz('可以看下你的手机吗？').then((answer)=>{
+    if(answer=='可以'){
+        quiz('走进你的心里可好！').then((answer)=>{
+            console.log(answer);
         })
     }
-});
-//Promise es6
+})
 
 
 
